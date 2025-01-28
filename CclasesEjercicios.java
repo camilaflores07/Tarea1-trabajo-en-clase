78cb4322262a36173ba182c9dc7de01f91c0f62b
Camila Flores <camila_maria@unitec.edu>
2025-01-28 14:39:24 -0600

    Add files via upload

---
+++ CclasesEjercicios.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cclases.ejercicios;
+
+/**
+ *
+ * @author 50494
+ */
+public class CclasesEjercicios {
+
+    
+    public static void main(String[] args) {
+      EmailAccount email = new EmailAccount("katherine.22@gmail.com");
+    }
+    
+}
